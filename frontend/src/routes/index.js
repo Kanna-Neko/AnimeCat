c0088ed1f74ed2b6c5840959edf9068828d8d685
她在偷看我 <oncwnuNaxhKeiNhpVvTCcsqdRR4g@git.weixin.qq.com>
2022-07-27 17:37:58 +0800

    init

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,19 @@
+import { Navigate } from 'react-router-dom'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+
+
+export default [
+  {
+    path: '/home',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/',
+    element: <Navigate to="/home" />
+  }
+]
